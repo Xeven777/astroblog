264795fba030dea65dfc57be2952f88f3032de02
Anish <xevenbiswas@gmail.com>
2023-12-26 17:59:07 +0530

    Update Astro Starter Kit: Blog configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,13 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 import jsx from '@vitejs/plugin-vue';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-blog7.netlify.app/",
   plugins: [jsx()],
-  integrations: [
-    mdx(),
-    sitemap(),
-    react({
-      include: ["**/react/*"],
-    }),
-  ],
+  integrations: [mdx(),sitemap(), react({
+    include: ["**/react/*"]
+  }), tailwind()]
 });
