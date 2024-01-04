0eb89b930458eda67967e243f57a590e2ebfd433
Anish <xevenbiswas@gmail.com>
2024-01-04 22:42:04 +0530

    added clarity

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 import jsx from '@vitejs/plugin-vue';
-
 import compress from "astro-compress";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +13,5 @@ export default defineConfig({
   plugins: [jsx()],
   integrations: [mdx(), sitemap(), react({
     include: ["**/react/*"]
-  }), compress()]
+  }), compress(), partytown()]
 });
